762daf40cd146c2583151bda2937bd4128a4a900
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-19 22:14:54 +0500

    Create PriceHistory Model structure

---
+++ PC_Parts_Scrapper/Models/PriceHistory.cs
@@ -0,0 +1,12 @@
+﻿namespace PC_Parts_Scrapper.Models
+{
+    public class PriceHistory
+    {
+        public int ScrapedItemId { get; set; }
+        public ScrapedItem? ScrapedItem { get; set; }   
+        public int PriceHistoryId {  get; set; }
+        public decimal Price {  get; set; }
+        public DateTimeOffset Date { get; set; }
+
+    }
+}
---
+++ PC_Parts_Scrapper/Models/ScrapedItem.cs
@@ -0,0 +1,14 @@
+﻿namespace PC_Parts_Scrapper.Models
+{
+    public class ScrapedItem
+    {
+        public int ScrapedItemId { get; set; }
+        public Uri? Url { get; set; }
+        public List<PriceHistory>? PriceHistory { get; private set; } = new List<PriceHistory>();  
+        public int StoreId { get; set; }
+        public Store? Store {  get; set; }
+        public int ProductId { get; set; }
+        public Product? Product { get; set; } 
+
+    }
+}
