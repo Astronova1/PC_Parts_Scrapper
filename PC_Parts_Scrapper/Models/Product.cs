12f44ef87b0b4ae3a8b9c99b26039526a258f075
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-19 20:52:02 +0500

    create Product model structure

---
+++ PC_Parts_Scrapper/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace PC_Parts_Scrapper.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public String Name { get; set; } = String.Empty;
+    }
+}
