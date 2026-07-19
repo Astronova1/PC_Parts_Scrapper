d4c2a53491800d30d36b7791ffc32edd1f1d80fe
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-19 20:39:30 +0500

    Add store model containing store information

--- PC_Parts_Scrapper/Models/Store.cs
+++ PC_Parts_Scrapper/Models/Store.cs
@@ -1,6 +1,10 @@
 ﻿namespace PC_Parts_Scrapper.Models
 {
+    // This class will contain properties about the store
     public class Store
     {
+        public int StoreId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public required Uri URL { get; set; }
     }
 }
