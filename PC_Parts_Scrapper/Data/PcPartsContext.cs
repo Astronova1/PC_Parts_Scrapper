428de776d7a30ec835411a706cddd59b29dce46e
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-21 19:39:09 +0500

    Create Dbcontext to connect db to app

---
+++ PC_Parts_Scrapper/Data/PcPartsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PC_Parts_Scrapper.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PC_Parts_Scrapper.Data
+{       //addDbContext registers the DbContext as scoped Lifetime i.e  services are created once per client request (connection).
+    public class PcPartsContext : DbContext  
+    {   
+        public PcPartsContext(DbContextOptions<PcPartsContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Product> Products { get; set; } 
+        public DbSet<ScrapedItem> ScrapedItems { get; set; }   
+        public DbSet<PriceHistory> PriceHistory { get; set; }
+    }
+}
