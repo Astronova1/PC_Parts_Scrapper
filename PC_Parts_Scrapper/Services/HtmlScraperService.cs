b6a5abe7abe4d6acdba297eca961a1c0228bb6bd
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-23 21:54:04 +0500

    implement playwright to circumvent javascript restrictions

--- PC_Parts_Scrapper/Services/HtmlScraperService.cs
+++ PC_Parts_Scrapper/Services/HtmlScraperService.cs
@@ -1,18 +1,27 @@
 ﻿using HtmlAgilityPack;
+using Microsoft.Playwright;
 namespace PC_Parts_Scrapper.Services
 {
     public class HtmlScraperService
     {
         public async Task Czone()
         {
-            var doc = new HtmlDocument();
-            doc.Load("ProcessorPricesinPakistan.htm");
+           
             //web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)";
             //HtmlDocument doc = await web.LoadFromWebAsync("https://www.czone.com.pk/processors-pakistan-ppt.85.aspx");
             //Console.WriteLine(doc.DocumentNode.OuterHtml.Length);
             //var pageTitle = doc.DocumentNode.SelectSingleNode("//title")?.InnerText.Trim();
             //Console.WriteLine($"Page Title: {pageTitle}");
-            var productsNds = doc.DocumentNode.SelectNodes("//div[contains(@class,'content-wrapper')]");
+            using var playwright = await Playwright.CreateAsync();        //here we initilize playwrite
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false }); //launc using chromium
+            var page = await browser.NewPageAsync();        //open new page
+            await page.GotoAsync("https://www.czone.com.pk/processors-pakistan-ppt.85.aspx");     //navigate to the link
+
+            string html_con = await page.ContentAsync();                   //store the html in html
+            //var productsNds = doc.DocumentNode.SelectNodes("//div[contains(@class,'content-wrapper')]");
+
+             //var doc = new HtmlDocument();
+            //doc.Load("ProcessorPricesinPakistan.htm");
 
             if (productsNds == null)
             {
