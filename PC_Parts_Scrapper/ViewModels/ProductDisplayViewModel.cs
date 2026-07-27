322b07976f16b9ab2a6f6a0c58870a3d1d4da551
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-27 22:43:00 +0500

    add Product ViewModel DTO

---
+++ PC_Parts_Scrapper/ViewModels/ProductDisplayViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PC_Parts_Scrapper.Views.ViewModels
+{
+    public class ProductDisplayViewModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<StoreListingViewModel> Listings { get; set; } = new();
+    }
+}
---
+++ PC_Parts_Scrapper/ViewModels/StoreListingViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PC_Parts_Scrapper.Views.ViewModels
+{
+    public class StoreListingViewModel
+    {
+        public string StoreName { get; set; } = string.Empty;
+        public Uri? Url { get; set; }
+        public decimal LatestPrice { get; set; }
+    }
+}
