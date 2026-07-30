5450e8d67cf4ecd616f30c06bd8d4a71f0432b2e
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-30 21:04:29 +0500

    Update ScrapedItem model to include full item title

---
+++ PC_Parts_Scrapper/Migrations/20260730155816_Update_ProductModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PC_Parts_Scrapper.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_ProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "ScrapedItems",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "ScrapedItems");
+        }
+    }
+}
