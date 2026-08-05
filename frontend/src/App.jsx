64467221bdd52af12425a69dd68474dcc01832c4
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-05 22:24:17 +0500

    create react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app-container'>
+      <h1>PC Price Tracker</h1>
+      <ProductList/>
+    </div>
+  )
+}
+
+export default App
