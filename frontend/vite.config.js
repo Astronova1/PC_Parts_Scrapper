426bd10197aa31543c84699004e13e62be351380
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-08 23:01:21 +0500

    update to use root domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/PC_Parts_Scrapper/", 
+  base: "/", 
 })
