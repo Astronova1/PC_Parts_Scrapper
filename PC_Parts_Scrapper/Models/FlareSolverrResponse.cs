c5cf6c2f3e265fa9fc5ee3e66e0a262b22f4823e
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-13 13:44:59 +0500

    support for error message for FlareSolverr

---
+++ PC_Parts_Scrapper/Models/FlareSolverrResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace PC_Parts_Scrapper.Models
+{
+    public class FlareSolverrResponse
+    {
+        public string Status { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public FlareSolverrSolution? Solution { get; set; }
+    }
+    public class FlareSolverrSolution
+    {
+        public string Response { get; set; } = string.Empty;
+        public int Status { get; set; }
+    }
+}
