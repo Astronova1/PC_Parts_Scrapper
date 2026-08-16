897c4f09b0a5c73ccb595d995958d04a903f55f8
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-16 20:19:48 +0500

    add category model and create one to many relationship

---
+++ PC_Parts_Scrapper/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace PC_Parts_Scrapper.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public ICollection<Product>? Products { get; set; }
+            
+    }
+}
--- PC_Parts_Scrapper/Models/Product.cs
+++ PC_Parts_Scrapper/Models/Product.cs
@@ -5,5 +5,6 @@
         public int ProductId { get; set; }
         public String Name { get; set; } = String.Empty;
         public ICollection<ScrapedItem> ScrapedItems { get; set; } = new List<ScrapedItem>();
+        public Category? Category { get; set; }
     }
 }
