ddd419bc63e60830bc095ad1012d16c5829227d6
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-17 14:33:45 +0500

    add category to a product

---
+++ PC_Parts_Scrapper/Utils/AddCategory.cs
@@ -0,0 +1,31 @@
+﻿namespace PC_Parts_Scrapper.Utils
+{
+    public static class AddCategory
+    {
+        public static string Addcategory(string productName)
+        {
+            List<string> gpus = new List<string> 
+            {
+                "RTX","GTX","GT","RADEON","RX","ARC"
+            };
+
+            List<String> cpus = new List<string>
+            {
+                "RYZEN","INTEL","CORE","AMD"
+            };
+            string upperName = productName.ToUpper();
+            if (gpus.Any(gpu => upperName.Contains(gpu)))
+            {
+                return "GPU";
+            }
+            else if (cpus.Any(cpu => upperName.Contains(cpu)))
+            {
+                return "CPU";
+            }
+            else
+            {
+                return "Other";
+            }
+        }
+    }
+}
