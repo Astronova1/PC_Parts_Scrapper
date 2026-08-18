8d1f3068c02c65201b8011d77d766cffe9fc23ef
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-18 20:23:13 +0500

    create category controller to return categories from the database to the frontend

---
+++ PC_Parts_Scrapper/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PC_Parts_Scrapper.Data;
+using PC_Parts_Scrapper.Models;
+
+namespace PC_Parts_Scrapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly PcPartsContext _context;
+
+        public CategoriesController(PcPartsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return Ok(categories);
+        }
+    }
+}
