2b514f5dcfb49689baa189a09d8e719c60ccbfa4
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-24 16:54:12 +0500

    add Model for price alerts

---
+++ PC_Parts_Scrapper/Models/PriceAlert.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PC_Parts_Scrapper.Models
+{
+    public class PriceAlert
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; } = null!;
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+        public decimal TargetPrice { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
