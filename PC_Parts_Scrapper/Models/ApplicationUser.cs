b25e34f11d2069876665a2795f7892463179ae38
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-24 16:38:49 +0500

    add model or user creation

---
+++ PC_Parts_Scrapper/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
+
+namespace PC_Parts_Scrapper.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<PriceAlert>? PriceAlerts { get; set; }
+    }
+}
