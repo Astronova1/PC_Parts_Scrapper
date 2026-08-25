ffdd2a705e7513df3faccabec130b66814566ed1
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-25 14:47:11 +0500

    add viewmodels for login , register and auth reponse

---
+++ PC_Parts_Scrapper/ViewModels/AuthResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace PC_Parts_Scrapper.ViewModels
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = new();
+        public DateTime ExpiresAt { get; set; }
+    }
+}
---
+++ PC_Parts_Scrapper/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PC_Parts_Scrapper.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
