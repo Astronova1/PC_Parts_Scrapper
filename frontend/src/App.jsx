ba545697b9e43e773d0af1bdef134e410cbbeb3c
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-27 02:09:30 +0500

    add routing for login register and future profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,36 @@ import heroImg from './assets/hero.png'
 import ProductList from './pages/ProductList'
 import './App.css'
 import Navbar from './components/Navbar'
-import {Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import ProductDetails from './pages/ProductDetails'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ProtectedRoute from './components/ProtectedRoute'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <><Navbar/>
+    <>
+    <AuthProvider>
+    <Navbar/>
       <div className='app-container'>
         <Routes>
           <Route path="/" element={<ProductList/>}/>
           <Route path="/products" element={<ProductList/>}/>
+          <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <div>Profile Page (Protected)</div>
+              </ProtectedRoute>
+            } />
           <Route path="/about" element={<div>About Page</div>}/>
           <Route path='/products/:id' element={<ProductDetails/>}></Route>
         </Routes>
       </div>
+      </AuthProvider>
   </>
   )
 }
