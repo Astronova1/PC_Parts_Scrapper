2e38e9a08d7b7fbc9c729119a4cb665948dfd610
Astronova1 <awaiz123nasir123@gmail.com>
2026-08-27 18:19:57 +0500

    create Dto for price alerts

---
+++ PC_Parts_Scrapper/ViewModels/CreateAlert.cs
@@ -0,0 +1,8 @@
+﻿namespace PC_Parts_Scrapper.ViewModels
+{
+    public class CreateAlert
+    {
+        public int ProductId { get; set; }
+        public decimal TargetPrice { get; set; }
+    }
+}
