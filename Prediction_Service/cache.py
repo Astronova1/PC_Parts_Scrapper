c6d6009f95a4afe953ebd347dedb035e5df5219c
Astronova1 <awaiz123nasir123@gmail.com>
2026-09-03 12:03:11 +0500

    implement simple memroy cache with TTL

--- Prediction_Service/cache.py
+++ Prediction_Service/cache.py
@@ -1,5 +1,22 @@
 import time
 from typing import Optional
 
-_Cache ={}
-CACHE_TTL = 36000
+_cache = {}
+CACHE_TTL = 3600  # 1 hour
+
+
+def get_cache(key: str):
+    if key in _cache:
+        data, expiry = _cache[key]
+        if time.time() < expiry:
+            return data
+        del _cache[key]
+    return None
+
+
+def set_cache(key: str, data):
+    _cache[key] = (data, time.time() + CACHE_TTL)
+
+
+def clear_cache():
+    _cache.clear()
